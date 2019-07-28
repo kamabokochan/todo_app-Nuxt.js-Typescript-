d018c542177920271f39a48e12a5893c9e814350
kamabokochan <caffe@harachan.local>
2019-07-28 19:04:24 +0900

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,64 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-ts-scaffold
-      </h1>
-      <h2 class="subtitle">
-        Scaffold for a project using Nuxt.js with TypeScript
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </section>
+  <div class="todoList">
+    <ul class="todoListWrap">
+      <li v-for="(item, index) in todoList" :key="`todo${index}`">
+        <p class="taskId">{{ index }}</p>
+        <p class="task">{{ item.task }}</p>
+        <p class="isDone">
+          <input id="" v-model="item.isDone" type="checkbox" name="" />
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import Logo from '~/components/Logo.vue'
 
+type todoType = {
+  id: number
+  task: string
+  isDone: boolean
+}
+
 @Component({
   components: {
     Logo
   }
 })
-export default class Index extends Vue {}
+export default class Index extends Vue {
+  todoList: todoType[] = [
+    {
+      id: 1,
+      task: 'タスク1',
+      isDone: false
+    },
+    {
+      id: 2,
+      task: 'タスク2',
+      isDone: false
+    },
+    {
+      id: 3,
+      task: 'タスク3',
+      isDone: false
+    }
+  ]
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+* {
+  margin: 0;
+  padding: 0;
+}
+ul,
+li {
+  list-style: none;
+}
+.todoListWrap > li {
   display: flex;
-  justify-content: center;
   align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
